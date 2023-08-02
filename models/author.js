325868ad01148aad27f909bd6b4c0155af6c345a
= <=>
2023-08-02 13:28:34 +0200

    Finished Author Routes

--- models/author.js
+++ models/author.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = mongoose.Schema({
     name: {
         type: String,
         required: true
     }
+})
+
+authorSchema.pre('deleteOne', {document: true}, function(next) {
+    Book.find({author: this.id})
+    .then((books) => {
+        if (books.length > 0){
+            throw new Error('This author has books still')
+        }
+        
+        next()
+    })
+    .catch(next)
 })
 
 module.exports = mongoose.model('Author', authorSchema)
--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express =  require('express')
 const router = express.Router()
 const Author = require('../models/author.js')
+const Book = require('../models/book.js')
 
 // All Authors Route
 router.get('/', async (req, res) => {
@@ -21,7 +22,7 @@ router.get('/', async (req, res) => {
 
 // New Authors Route
 router.get('/new', (req, res) => {
-    res.render('authors/new', {author: new Author} )
+    res.render('authors/new', {author: new Author()} )
 })
 
 // Create Author Route
@@ -32,11 +33,75 @@ router.post('/', (req, res) => {
 
     author.save()
     .then((newAuthor) => {
-        // res.redirect(`authors/${newAuthor.id}`)
-        res.redirect(`authors`)
+        res.redirect(`authors/${newAuthor.id}`)
     })
     .catch((err) => {
         res.render('authors/new', {author: author, errorMessage: 'Error creating author...'})
+    })
+})
+
+// Show Author
+router.get('/:id', (req, res) => {
+    Author.findById(req.params.id)
+    .then((foundAuthor) => { Book.find({author:foundAuthor.id}).limit(6).exec()
+        .then((foundBooks) => {
+            res.render('authors/show', {author: foundAuthor, booksByAuthor: foundBooks})
+        })
+    })
+    .catch(() => {
+        res.redirect('/')
+    })
+})
+
+// Edit Author
+router.get('/:id/edit', (req, res) => {
+    Author.findById(req.params.id)
+    .then((author) => {
+        res.render('authors/edit', {author: author} )
+    })
+    .catch(() => {
+        res.redirect('/authors')
+    })
+})
+
+// Update Author
+router.put('/:id', (req, res) => {
+    let author
+
+    Author.findById(req.params.id)
+    .then((foundAuthor) => {
+        author = foundAuthor;
+        foundAuthor.name = req.body.name
+        foundAuthor.save()
+        .then(() => res.redirect(`/authors/${author.id}`))
+        .catch(() => res.redirect('/authors/edit',  {author: author, errorMessage: 'Error updating Author...'}))
+    })
+    .catch((err) => {
+        res.render('/')
+    })
+})
+
+// Delete author
+router.delete('/:id', (req, res) => {
+    let author
+
+    Author.findById(req.params.id)
+    .then((foundAuthor) => {
+        author = foundAuthor;
+        foundAuthor.deleteOne()
+        .then(() => {
+            res.redirect('/authors')
+        })
+        .catch((err) => {
+            console.log(err)
+            res.redirect(`/authors/${author.id}`)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+        if(author == null){
+            res.redirect('/')
+        }
     })
 })
 
